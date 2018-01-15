0192fc5eedd47f46c2f8bc35f135fefcce6e7d70
Jonathan Dust <d24dust@hotmail.com>
2018-01-15 20:55:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |item|
+  array.each do |item|
     cheese_types.each { |cheese| return item if item == cheese }
   end
   return nil
